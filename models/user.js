8a719a0b40bf6cfebab0f45347b4ad83739f560b
james <engrjayt200@gmail.com>
2020-12-07 00:45:04 +0100

    Updated User.js

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date.now,
+        default: Date.now
+    }
+});
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
+//Import models
+const user = require("../models/user");
+
 // Login handle
 router.get("/login", (req, res)=>{
     res.render("login");
 })
 
 router.get("/register", (req, res)=>{
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+    console.log(" Name " + name + " email :" + email + " pass:" + password);
+    if(!name || !email || !password || !password2){
+        errors.push({msg: "Please fill in all field"})
+    }
     res.render("Register")
 })
 
