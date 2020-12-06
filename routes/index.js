3b7c7404315f2bdee5c8a318224a478b03fd9318
james <engrjayt200@gmail.com>
2020-12-06 22:36:59 +0100

    Implement routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
 const router = express.Router();
 // Login
+router.get("/", (req, res)=>{
+    res.render("Welcome to my app");
+})
+
+//Register page
+router.get("/register", (req, res)=>{
+    res.render("Register")
+})
+
+module.exports = router;
